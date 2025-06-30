2b82d1e5b5eea9e407adef1ddcff4f9ae8dc5032
Sushey01 <susmagar012@gmail.com>
2025-06-30 23:33:36 +0545

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,30 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
-import Contact from "./components/Contact"
+import Contact from "./components/Contact";
 import FeaturedProject from './components/FeaturedProject';
+import Pivotal from './components/Pivotal';
+import Tangerine from './components/Tangerine';
+import Footer from "./components/Footer";
+import Quote from './components/Quote'; // Make sure this is imported!
 
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HeroSection />} />
+        <Route path="/" element={
+          <>
+            <HeroSection />
+            <Quote />
+            <FeaturedProject />
+            <Footer/>
+          </>
+        } />
         <Route path="/projects" element={<FeaturedProject />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/pivotal" element={<Pivotal />} />
+        <Route path="/tangerine" element={<Tangerine />} />
       </Routes>
     </BrowserRouter>
   );
