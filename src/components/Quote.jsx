8f56be3404c7ec274956c3e1e57099ea7b99cc80
Sushey01 <susmagar012@gmail.com>
2025-07-02 23:25:31 +0545

    delte

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -4,12 +4,12 @@ import QuoteImage from "../assets/images/quote.png"
 
 
 const Quote = () => {
-
+const navigate=useNavigate("")
 
   return (
     <>
     <div className="quote-img">
-        <img src={QuoteImage}></img>
+        <img src={QuoteImage} onClick={navigate("/")}></img>
         </div>
         </>
          
